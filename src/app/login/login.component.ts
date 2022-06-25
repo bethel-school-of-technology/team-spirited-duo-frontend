c49dd9db5a4248cbf923262cbb2ab617d7b9e6b6
Roland Shorter <rolandshorter@yahoo.com>
2022-06-25 15:37:21 -0400

    Finished Signup and Login components with routing and validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
-  }
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: ['']
+    })
 
+  }
+  login(){
+    this.http.get<any>("http://localhost:3000/userSignUp")
+    .subscribe(res=>{
+      const user = res.find((a:any)=>{
+        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+      if (user){
+        alert("You have successfully logged in!");
+        this.loginForm.reset();
+        this.router.navigate(['catgory-list'])
+      }else{
+        alert("User not found!");
+      }
+    })
+  }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder} from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  public signupForm !: FormGroup;
+  
+  constructor(private formBuilder: FormBuilder, private http : HttpClient, private router: Router) { }
 
   ngOnInit(): void {
-  }
+    this.signupForm = this.formBuilder.group({
+      fullname: [''],
+      phoneNumber: [''],
+      email: [''],
+      password: [''],
+
+    
+    })
+
+}
+signUp() {
+  this.http.post<any>("http://localhost:3000/userSignUp", this.signupForm.value)
+  .subscribe(res=>{
+    alert("You have successfully signed up");
+    this.signupForm.reset();
+    this.router.navigate(['login']);
+  })
+} 
 
 }
